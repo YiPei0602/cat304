ff4e664306120ba956a1f3a5d8fb193201e71929
ceeee33 <118290498+ceeee33@users.noreply.github.com>
2025-01-07 19:25:31 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,19 +3,43 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserRoleSelection from './pages/Auth/UserRoleSelection';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
-import Dashboard from './pages/Dashboard/Dashboard';
 import './App.css';
+import ResourceRequestForm from './pages/Student/requestform';
+import StudentDashboard from './pages/Student/studentDashboard';
+import AdminDashboard from './pages/admin/adminDashboard';
+import Inventory from './pages/admin/inventory';
+import QRScanner from './pages/admin/QRScanner';
+import DonorDashboard from './pages/donor/donorDashboard';
+import ItemList from './pages/Student/itemList';
+import DonationHistory from './pages/donor/DonationHistory';
+import CollectionHistory from './pages/Student/collectionHistory';
+import DonorApplication from "./pages/Review/DonorApplication";
+import StudentApplication from './pages/Review/StudentApplication';
 
 function App() {
+
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<UserRoleSelection />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/requestform" element={<ResourceRequestForm />} />
+        <Route path="/studentDashboard" element={<StudentDashboard/>}/>
+        <Route path="/adminDashboard" element = {<AdminDashboard/>}/>
+        <Route path="/donorDashboard" element = {<DonorDashboard/>}/>
+        <Route path="/itemList" element ={<ItemList/>}/>
+        <Route path="/inventory" element ={<Inventory/>}/>
+        <Route path="/qrscanner" element ={<QRScanner/>}/>
+        <Route path="/donor/history" element={<DonationHistory />} />
+        <Route path="/collectionHistory" element ={<CollectionHistory/>}/>
+        <Route path="/student-application" element={<StudentApplication />} />
+        <Route path="/donor-application" element={<DonorApplication />} />
+
       </Routes>
     </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'; // Your custom CSS
 import 'antd/dist/reset.css'; // Ant Design styles (use 'antd/dist/antd.css' for v4 or earlier)
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
 import App from './App';
+// import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
